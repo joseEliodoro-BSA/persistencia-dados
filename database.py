0a8821a1c79e915aa2139663a68175f2eb34c7b1
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-06 17:39:33 -0300

    feat: criando conexão com o banco de dados

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "mysql://root:root@localhost:3306/persistencia"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine) # conexão entre o banco é o código
+
+Base = declarative_base()
