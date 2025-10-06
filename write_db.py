d3a3845bb52773f4761e9d95790681d28e388d7e
Jose Eliodoro <jose.santana@bsatech.io>
2025-10-06 14:55:14 -0300

    feat: atualizando write_db para adicionar disciplina e subindo bando

--- write_db.py
+++ write_db.py
@@ -3,10 +3,16 @@ import sqlite3
 conn = sqlite3.connect("escola.db")
 cursor = conn.cursor()
 
+# cursor.execute(
+#     """
+#     INSERT INTO estudantes(nome, idade) VALUES (?, ?)
+#     """,  ("Anna", 19)
+# )
+
 cursor.execute(
     """
-    INSERT INTO estudantes(nome, idade) VALUES (?, ?)
-    """,  ("Anna", 19)
+    INSERT INTO disciplinas(nome, id_estudante) VALUES (?, ?)
+    """,  ("discreta", 1)
 )
 
 conn.commit()
